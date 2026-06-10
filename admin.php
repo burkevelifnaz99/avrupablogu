3710b50c35988324973ce1ff74aa4503144f2dd3
burkevelifnaz99 <burkevelifnaz@gmail.com>
2026-06-10 10:15:45 +0300

    Add files via upload

--- admin.php
+++ admin.php
@@ -10,17 +10,29 @@
     
 </body>
 </html>
+<?php
+session_start();
+
+if(!isset($_SESSION['admin'])){
+    header("Location: admingiris.php");
+    exit;
+}
+?>
 
 <?php
+
 include "baglan.php";
 
+// Mesaj silme işlemi
 if(isset($_GET['sil'])){
     $id = $_GET['sil'];
     mysqli_query($conn, "DELETE FROM iletisim WHERE id=$id");
 }
 
+// Başlık
 echo "<h1>📝 Gelen Mesajlar</h1>";
 
+// Mesajları çek
 $result = mysqli_query($conn, "SELECT * FROM iletisim ORDER BY id DESC");
 while($row = mysqli_fetch_assoc($result)){
     echo "<div class='mesaj-kart'>";
@@ -28,6 +40,7 @@ while($row = mysqli_fetch_assoc($result)){
     echo "<p>".$row['mesaj']."</p>";
     echo "<small>".$row['email']." | ".$row['tarih']."</small>";
     echo "<br><a class='sil-btn' href='admin.php?sil=".$row['id']."' onclick='return confirm(\"Silinsin mi?\")'>Sil</a>";
+
     echo "</div>";
 }
 ?>
